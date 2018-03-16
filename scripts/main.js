500728ea2969ed6d3d9a0e782a21202524dd1d97
jxzhe <wuyenche@gmail.com>
2018-03-17 03:46:16 +0800

    clean it up

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,16 @@
 setInterval(navOffset, 500);
 function navOffset() {
     navbarNav = document.getElementById('navbarNav');
-    nav = document.getElementsByClassName('nav-offset');
+    sections = document.querySelectorAll('main section')
     if (navbarNav.classList.contains('show')) {
-        for (var i = 0; i < nav.length; ++i) {
-            nav[i].style.marginTop = '-13rem';
-            nav[i].style.paddingTop = '13rem';
+        for (var i = 0; i < sections.length; ++i) {
+            sections[i].style.marginTop = '-13rem';
+            sections[i].style.paddingTop = '13rem';
         }
-        document.body.setAttribute('data-offset', '144'); // somehow not working
     } else {
-        for (var i = 0; i < nav.length; ++i) {
-            nav[i].style.marginTop = '-4rem';
-            nav[i].style.paddingTop = '4rem';
+        for (var i = 0; i < sections.length; ++i) {
+            sections[i].style.marginTop = '-3.5rem';
+            sections[i].style.paddingTop = '3.5rem';
         }
-        document.body.setAttribute('data-offset', '0'); // somehow not working
     }
 }
