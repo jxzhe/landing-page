28ce5dd2dd81140a2fca89cf87819fc0d19258b6
jxzhe <wuyenche@gmail.com>
2018-03-17 06:59:02 +0800

    optimize main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,19 @@
-setInterval(navOffset, 500);
+var flag = false;
 
 function navOffset() {
     navbarNav = document.getElementById('navbarNav');
-    if (navbarNav.classList.contains('show')) {
-        document.styleSheets[0].addRule('main section::before', 'top: 13rem !important;');
-        document.styleSheets[0].addRule('main section::after', 'top: 13rem !important;');
-        document.styleSheets[0].addRule('main section', 'margin-top: -13rem !important;');
-        document.styleSheets[0].addRule('main section', 'padding-top: 14rem !important;');
-    } else {
-        document.styleSheets[0].addRule('main section::before', 'top: 3.5rem !important;');
-        document.styleSheets[0].addRule('main section::after', 'top: 3.5rem !important;');
-        document.styleSheets[0].addRule('main section', 'margin-top: -3.5rem !important;');
-        document.styleSheets[0].addRule('main section', 'padding-top: 4.5rem !important;');
+    if (flag === false && navbarNav.classList.contains('show')) {
+        document.styleSheets[0].addRule('main section::before', 'top: 13rem;');
+        document.styleSheets[0].addRule('main section::after', 'top: 13rem;');
+        document.styleSheets[0].addRule('main section', 'margin-top: -13rem;');
+        document.styleSheets[0].addRule('main section', 'padding-top: 14rem;');
+        flag = true;
+    } else if (flag === true && !navbarNav.classList.contains('show')) {
+        document.styleSheets[0].addRule('main section::before', 'top: 3.5rem;');
+        document.styleSheets[0].addRule('main section::after', 'top: 3.5rem;');
+        document.styleSheets[0].addRule('main section', 'margin-top: -3.5rem;');
+        document.styleSheets[0].addRule('main section', 'padding-top: 4.5rem;');
+        flag = false;
     }
 }
+setInterval(navOffset, 500);
